7336cdbfb30957d31ecb65691252c3b47c7f0169
Budddhika Rathnayake <rathnayakearmb@gmail.com>
2025-11-10 23:25:46 +0530

    create main method

--- SelectionSort.java
+++ SelectionSort.java
@@ -14,4 +14,23 @@ public class SelectionSort {
             }
         }
     }
+    public static void printArray(int[]arr)
+	{
+		for(int i=0;i<arr.length; i++)
+		{
+			System.out.print(arr[i]+" ");
+		}
+		System.out.println();
+	}
+
+    public static void main(String []args){
+        int[] array = {7,5,3,8,6,4,10,2,9,1};
+        System.out.println("Array before sorting:");
+        printArray(array);
+
+        selectionSort(array);
+        
+        System.out.println("Array after sorting:");
+        printArray(array);
+    }
 }
