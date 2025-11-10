a51d9e927482eb29ddf5b2652ea583500d2ec73c
Budddhika Rathnayake <rathnayakearmb@gmail.com>
2025-11-10 23:20:20 +0530

    create array printing and  main method

--- BubbleSort.java
+++ BubbleSort.java
@@ -21,4 +21,22 @@ public class BubbleSort{
         }
 
     }
+
+    public static void printArray(int[] array){
+        for(int i=0 ; i<array.length ; i++){
+            System.out.print(array[i]+ " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String []args){
+        int[] array = {7,5,3,8,6,4,10,2,9,1};
+        System.out.println("Array before sorting:");
+        printArray(array);
+
+        bubbleSort(array);
+        
+        System.out.println("Array after sorting:");
+        printArray(array);
+    }
 }
